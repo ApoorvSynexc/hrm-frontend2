801898160dacb2177afca50e1c8cc536a1294131
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-07-13 18:30:08 +0530

    initial commit

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -34,7 +34,7 @@ export function Sidebar({ collapsed, onToggleCollapsed }: SidebarProps) {
   return (
     <aside
       className={`relative flex shrink-0 flex-col border-r border-border bg-surface transition-[width] duration-200 ${
-        collapsed ? 'w-16' : 'w-60'
+        collapsed ? 'w-16' : 'w-44'
       }`}
     >
       <div className="flex h-16 items-center gap-2.5 px-4">
